36f33b38e3bf10fc458d2ba0684aa6a210cc88f4
marlissonls <marlisson.business@gmail.com>
2022-10-05 12:53:38 -0300

    chore: remoção do console.log em controllers/user.js

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -6,7 +6,6 @@ export async function createUser(req, res) {
     const { name, email, password } = req.body;
     const result = await userServices.createUser(name, email, password);
     if (result.error === null) {
-        console.log (`userID: ${userID}, userType: ${userType}`);
         res.status(200).json({ message: 'user created succesfully!' });
     } else {
         res.status(result.status).json({ message: result.error });
