4262c07f5af944250df2f31c671e59612db8b12b
Fabio-Cesar <fabiocesar_33@outlook.com>
2022-10-10 13:26:33 -0300

    clear input values

--- public/static/views/signup.js
+++ public/static/views/signup.js
@@ -6,4 +6,14 @@ export class SignupView extends View {
         super();
         this.display = "#signup-page";
     };
+
+    async getData() {
+        try {
+            document.querySelector('#signup-input-email').value = '';
+            document.querySelector('#signup-input-password').value = '';
+            document.querySelector('#signup-input-user').value = '';
+        } catch (error) {
+            console.log(error)
+        }
+    }
 };
